c5d1481e59028acf52bf56ba0fc16d509131c000
Shubhangi <sbisaria@tavisca.com>
2017-08-24 15:15:22 +0530

    automobile manager

---
+++ AutomobileServiceProvider/AutomobileServiceProvider/VehicleManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileServiceProvider
+{
+    public class VehicleManager
+    {
+        public List<Vehicle> Vehicles { get; set; }
+        public List<Vehicle> SoldVehicle { get; set; }
+        public List<Vehicle> RentedVehicles { get; set; }
+        
+        public bool AddVehicle()
+        {
+           
+            return true;
+        }
+        public bool RemoveVehicle()
+        {
+            
+            return true;
+        }
+        public List<string> GetRentableVehicle()
+        {
+            foreach (vehicle in Vehicle)
+                return ;
+
+        }
+        public List<Vehicle> GetRentedVehicle()
+        {
+
+        }
+        public List<Vehicle> GetSoldVehicle()
+        {
+            return "";
+        }
+    }
+}
